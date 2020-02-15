13ce4e5c67f49e9540b1661acc6c234503b3995c
Shivakumar1406 <shivukati14@gmail.com>
2020-02-15 12:06:21 +0530

    Updates

---
+++ precache-manifest.c192619fad872b7217d573ba7fb1f63c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163a0970540930d6f3630884915f0726",
+    "url": "/Shivakumar1406/cart.git/index.html"
+  },
+  {
+    "revision": "7a7296c0b3ae00125de8",
+    "url": "/Shivakumar1406/cart.git/static/css/main.1c020371.chunk.css"
+  },
+  {
+    "revision": "c1384bb9d7d33dfe3222",
+    "url": "/Shivakumar1406/cart.git/static/js/2.26a8ab3b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Shivakumar1406/cart.git/static/js/2.26a8ab3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7296c0b3ae00125de8",
+    "url": "/Shivakumar1406/cart.git/static/js/main.3b846d01.chunk.js"
+  },
+  {
+    "revision": "c379dcf04ba4da45e3c0",
+    "url": "/Shivakumar1406/cart.git/static/js/runtime-main.ba70af17.js"
+  }
+]);
